ec8bfed2485ad28b5e2787759fda5e6a84fddcd9
Eunhyuk Kim <gunoodaddy@gmail.com>
2012-09-16 03:23:26 +0900

    file deleted

--- SharedPaintServer/ProtocolManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ProtocolManagerImpl.h"
-
-class SessionClient;
-class OldGameTalkProtocol;
-
-typedef ProtocolManagerImpl<SessionClient, boost::shared_ptr<OldGameTalkProtocol> > ProtocolManager;
-

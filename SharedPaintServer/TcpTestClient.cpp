d28e287b3c534c4b9b7976ba0cf06a9984f2548c
Eunhyuk Kim <gunoodaddy@gmail.com>
2012-09-17 22:31:51 +0900

    missing file add

---
+++ SharedPaintServer/TcpTestClient.cpp
@@ -0,0 +1,8 @@
+#include "Coconut.h"
+#include "TcpTestClient.h"
+
+void TcpTestClient::onConnected( void ) {
+	LOG_DEBUG("TcpTestClient::onConnected AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+	// TODO SEND SYN
+}
---
+++ SharedPaintServer/TcpTestClient.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class TcpTestClient : public coconut::ClientController
+{
+	void onConnected();
+};
+
+

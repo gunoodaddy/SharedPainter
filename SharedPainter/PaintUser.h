7eeccaa9e8383330c267ea3d7e2d91c134b5a714
nb11436 <eunhyuck.kim@nhn.com>
2012-09-24 23:57:55 +0900

    initiall superpeer candidate value changed to false

--- SharedPainter/PaintUser.h
+++ SharedPainter/PaintUser.h
@@ -4,7 +4,7 @@
 
 struct SPaintUserInfoData
 {
-	SPaintUserInfoData() : listenTcpPort(0), superPeerCandidate(true/*TODO RELAYMODE*/) { }
+	SPaintUserInfoData() : listenTcpPort(0), superPeerCandidate(false) { }
 
 	std::string channel;
 	std::string userId;
